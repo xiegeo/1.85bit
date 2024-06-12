c51ff3f79d10ceb2bb6529e7f3a9306cd21baa4c
xiegeo <george@xiegeo.com>
2024-06-12 11:55:35 +0800

    edit story.py

--- story.py
+++ story.py
@@ -8,7 +8,6 @@ from modeling_bitnet import BitnetForCausalLM
 from tokenization_bitnet import BitnetTokenizer 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-from models import bitnet_64_2, tiny_stories_ref
 
 device = torch.device("cpu")
 if torch.cuda.is_available():
@@ -16,13 +15,13 @@ if torch.cuda.is_available():
 print(f'use {device}')
 
 tokenizer = AutoTokenizer.from_pretrained("roneneldan/TinyStories-1M")
-model = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-1M")
+model = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-33M")
 # load model from local file
 #model = AutoModelForCausalLM.from_pretrained("tiny_stories_ref", local_files_only=True)
 
 model.to(device)
 # use the model to generate a story
-for text in ["Once","Alice and Bob", "In a galaxy far far away"]:
+for text in ["Once","Alice and Bob", "In a galaxy far far away", "The lazy dog"]:
     inputs = tokenizer(text, return_tensors='pt').to(device)
     outputs = model.generate(**inputs, max_length=64)
     decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
