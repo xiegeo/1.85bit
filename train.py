c7024b60f677c24d9ee0c813352ed0f5e9dd18ac
xiegeo <george@xiegeo.com>
2024-11-04 03:35:41 +0800

    change learning rate

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def SGDFun(lr=1e-3):
     return fn
 
 class DynamicLearningRate(_LRScheduler):
-    def __init__(self, optimizer: Optimizer, lr_decay=0.99, decision_steps=20, lr_min=1e-6):
+    def __init__(self, optimizer: Optimizer, lr_decay=0.8, decision_steps=100, lr_min=1e-6):
         self.base_lrs = [group['lr'] for group in optimizer.param_groups]
         self.current_lr = self.base_lrs[0]
         self.lr_decay = lr_decay
