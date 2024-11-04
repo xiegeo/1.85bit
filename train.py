a1542bac6c39fec5409b511bc89a9e426937eedf
xiegeo <george@xiegeo.com>
2024-11-04 22:53:15 +0800

    increase swap width

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ def SGDFun(lr=1e-3):
     return fn
 
 class DynamicLearningRate(_LRScheduler):
-    def __init__(self, optimizer: Optimizer, lr_decay=0.9, lr_ratio=1/2, slow_start=1, swap_width=5, swaps=2, lr_min=1e-6):
+    def __init__(self, optimizer: Optimizer, lr_decay=0.9, lr_ratio=0.8, slow_start=1, swap_width=50, swaps=1, lr_min=1e-6):
         self.base_lrs = [group['lr'] for group in optimizer.param_groups]
         self.lr_max = self.base_lrs[0] 
         self.current_lr = self.lr_max * slow_start
