31d860011cd12650f84cf723c1ea831084f53785
xiegeo <george@xiegeo.com>
2024-11-10 19:35:41 +0800

    fix stochastic rounding during inference

--- utils_quant.py
+++ utils_quant.py
@@ -155,7 +155,7 @@ class BitLinear(nn.Linear):
         else:            
             quant_input = activation_quant(input)
             if stochastic_rounding:
-                quant_weight = weight_quant(self.weight)
+                quant_weight = stochastic_weight_quant(self.weight)
             elif self.quant_weight is None:
                 # quantize weight only once for inference, if it is stable
                 self.quant_weight = weight_quant(self.weight)
